ccaa1e6253e16bd8e846cf7921885880d82a6694
Ivan Flores <ifsilvar4@gmail.com>
2021-02-03 00:34:38 -0800

    wrote code that will get api data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import logo from './logo.svg';
 import './App.css';
 import OmdbContainer from "./components/OmdbContainer";
+
+
 
 function App() {
   return (
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASEURL = "https://randomuser.me/api/";
+const BASEURL = "https://randomuser.me/api/?results=10&inc=name,registered&nat=fr";
 // const APIKEY = "&apikey=trilogy";
 
 export default {
